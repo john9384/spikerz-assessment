0a8c291cc572de5a1f6cf06ae8b5ed6877ae3938
John Ogungbure <jogungbure@gmail.com>
2025-09-02 23:16:24 +0100

    implemented the initial flow look

---
+++ src/app/features/dashboard/components/right-panel/components/contextual-risk/contextual-risk.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'contextual-risk',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contextual-risk.component.html',
+  styleUrls: ['./contextual-risk.component.scss'],
+})
+export class ContextualRiskComponent {}
