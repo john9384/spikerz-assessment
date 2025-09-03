ae492095a72785e2c3b8a6ac42d6361d54a79400
John Ogungbure <jogungbure@gmail.com>
2025-09-03 01:07:45 +0100

    made the server icon reusable

--- src/app/features/remediation/remediation.component.ts
+++ src/app/features/remediation/remediation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
+import { ServerIconComponent } from '../../shared/components/ui/server-icon';
 
 @Component({
   selector: 'app-remediation',
   standalone: true,
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, TranslatePipe, ServerIconComponent],
   templateUrl: './remediation.component.html',
   styleUrls: ['./remediation.component.scss'],
 })
