d69daa053e716fdbf8ab1cf8dc8141c5bc219ae4
John Ogungbure <jogungbure@gmail.com>
2025-09-13 01:43:56 +0100

    updated the color of the nodes to match the design

--- src/app/features/dashboard/components/right-panel/components/asset-flow/graph-node/graph-node.component.ts
+++ src/app/features/dashboard/components/right-panel/components/asset-flow/graph-node/graph-node.component.ts
@@ -13,4 +13,7 @@ export class GraphNodeComponent {
   @Input() title: string = '';
   @Input() icon: string | null = null;
   @Input() position: 'top' | 'bottom' | 'left' | 'right' = 'top';
+  @Input() errorIcon: boolean = false;
+  @Input() userIcon: boolean = false;
+  @Input() initialNode: boolean = false;
 }
